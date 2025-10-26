b07bb01fa0c497cf7c55438cd129a75af7a3804e
karim hamed <143779715+karimhamed55@users.noreply.github.com>
2025-10-26 17:22:50 +0300

    Create employee class in TCPdata namespace

---
+++ TCPdata/employees.cs
@@ -0,0 +1,12 @@
+namespace TCPdata  // Changed from TCPdataa
+{
+    public class employee
+    {
+        public int id { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public decimal annualsalary { get; set; }
+        public bool ismanager { get; set; }
+        public int departmentid { get; set; }
+    }
+}
